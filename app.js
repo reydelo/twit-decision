d242f80cd7a4ab974da0454b54c814cc88947bf7
reydelo <reyna.deloge@gmail.com>
2015-08-26 14:09:59 -0600

    change templating engine from jade to swig

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+// *** view engine *** //
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
