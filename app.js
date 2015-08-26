891ed2fbd1174b1e020adaf0f818103d7450caca
reydelo <reyna.deloge@gmail.com>
2015-08-26 14:29:22 -0600

    install twit and Sentimental dependencies, updat app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var swig = require('swig');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// module dependencies
+var express = require('express'),
+  routes = require('./routes/index'),
+  http = require('http'),
+  path = require('path'),
+  fs = require('fs'),
+  swig = require('swig');
 
 var app = express();
 
@@ -19,16 +16,9 @@ app.set('view engine', 'html');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
